0dd2331c3ab2e360d143c01c526a80afd2bded80
Thing17 <yeungkaiwen@gmail.com>
2025-07-15 12:18:50 -0400

    made sprites; made 3 lvls; death and next lvl scripts

---
+++ Assets/Geo_Quest/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public int speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
+    }
+}
