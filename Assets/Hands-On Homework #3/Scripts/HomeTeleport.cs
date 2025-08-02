9ec76a43dcec71f63b186a424c1b28e8c8a77b1d
Thing17 <yeungkaiwen@gmail.com>
2025-08-01 21:00:25 -0400

    working on HW3 teleporter

---
+++ Assets/Hands-On Homework #3/Scripts/HomeTeleport.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Teleport : MonoBehaviour
+{
+    public string sceneName;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Player":
+                SceneManager.LoadScene(sceneName);
+                break;
+        }
+    }
+}
